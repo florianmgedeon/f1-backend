8a96658af8afe912b90f51b3d5544c8bfeb689a5
fged03 <florian.gedeon@gmail.com>
2025-04-11 19:46:59 +0200

    testing mode

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ let latestData = null;
 setInterval(async () => {
   try {
     const since = new Date(Date.now() - 2000).toISOString();
-    const res = await axios.get(`https://api.openf1.org/v1/car_data?driver_number=1&session_key=latest&date>${since}`);
+    //const res = await axios.get(`https://api.openf1.org/v1/car_data?driver_number=1&session_key=latest&date>${since}`);
+    //testing only:
+    const res = await axios.get(`https://api.openf1.org/v1/car_data?driver_number=55&session_key=9159&speed%3E=315`);
     const data = res.data;
     if (data.length > 0) latestData = data[data.length - 1];
   } catch (err) {
-    console.error('OpenF1 fetch error:', err.message);
+    console.error('OpenF1 fetch error in index.js line 21:', err.message);
   }
 }, 500);
 
